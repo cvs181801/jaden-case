6c85205f42d8404a8b3bc65be889b3712a81c608
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-17 16:34:42 -0700

    add basic js and html

---
+++ index.js
@@ -0,0 +1,17 @@
+//grab the elements
+
+//const randomText = document.getElementById("textBox");
+// const goBtn = document.getElementById("go-btn");
+// const resultDiv = document.getElementById("parentDiv");
+// const result = document.createElement("p");
+
+// create a way for the words in any random sentence to be automatically capitalized.
+
+const quote = "happy little critters abound.";
+
+function jadenCase(text) {
+    //var text = randomText.value;
+    return text.split('').map(capitalize).join('');
+}
+
+console.log(jadenCase(quote));
