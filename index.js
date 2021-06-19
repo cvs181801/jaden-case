ba279f5fd65781b191706faa87e925d4a03bf81f
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-19 11:50:04 -0700

    simplified the callback

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ function capitalizeAll(text) {
     .toLowerCase()
     .split(' ')
     .forEach(word =>
-        //word[0].toUpperCase + word.substring(1));
-        word.charAt(0).toUpperCase()+ word.slice(1).toLowerCase());
+        word[0].toUpperCase); //+ word.substring(1));
+        //word.charAt(0).toUpperCase()+ word.slice(1).toLowerCase());
          
     //.join(' ');
         
