175a09288ea8a5aa63ee99434d21130b5523e5f6
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-19 12:10:32 -0700

    try taking out .join()

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ function capitalizeAll(text) {
     .toLowerCase()
     .split(' ')
     .forEach((word) => {
-        console.log(word.charAt(0).toUpperCase() + word.substring(1))})//);
+        console.log(word.charAt(0).toUpperCase() + word.substring(1))});
         //word.charAt(0).toUpperCase()+ word.slice(1).toLowerCase());
          
-    .join(' ');
+    //.join(' ');
         
 }
 
