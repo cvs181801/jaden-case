6b6af6a25259aeba6d093637ef97e7ce0d0d0595
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-19 12:26:52 -0700

    try adding index

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function capitalizeAll(text) {
     .split(' ')
     .forEach((word) => {
         console.log(word.charAt(0).toUpperCase() + word.substring(1));
-        console.log(word);
+        console.log(word[i]);
         })//.join(' ');
         //word.charAt(0).toUpperCase()+ word.slice(1).toLowerCase());
         
