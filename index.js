9194f6602c3a541d1c8b8ea2e6ca4d2ba1598eee
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-18 17:39:14 -0700

    try using es6 syntax

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function capitalizeAll(text) {
     .map(function(word) {
         //console.log("First capital letter " + word[0]);
         //console.log("remaining letters: " + word.substring(1));
-        return word[0].toUpperCase + word.substring(1);
+        return `${word[0].toUpperCase} ${word.substring(1)}`;
     }).join(' ');
 
 }
